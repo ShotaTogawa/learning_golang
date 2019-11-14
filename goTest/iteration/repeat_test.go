4cf3253ea01b5a48d541a3d005cf4b2696b53c26
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-14 15:22:05 -0800

    finished iteration part

--- goTest/iteration/repeat_test.go
+++ goTest/iteration/repeat_test.go
@@ -10,3 +10,39 @@ func TestRepeat(t *testing.T) {
 	}
 }
 
+func Repeat(char string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += char
+	}
+	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+func Sum(nums ...int) int {
+	var total int
+	for _, v := range nums {
+		total += v
+	}
+	return total
+}
+
+func TestSum(t *testing.T) {
+	want := 15
+	expected := Sum(1,2,3,4,5)
+
+	if want != expected {
+		t.Errorf("expected %d, but got %d", expected, want)
+	}
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum(1,2,3,4,5)
+	}
+}
